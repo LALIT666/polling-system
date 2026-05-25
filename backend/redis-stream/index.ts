eebf3d5a883d80169d68114ec99b8383a6a71c12
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-05-25 03:14:52 +0200

    xAddBulk

--- backend/redis-stream/index.ts
+++ backend/redis-stream/index.ts
@@ -24,3 +24,13 @@ await client.connect();
 export async function xAdd(data: WebsiteEvent) {
   return await client.xAdd(STREAM_NAME, "*", data);
 }
+
+export async function xAddBulk(websites: WebsiteEvent[]) {
+  const pipeline = client.multi();
+
+  for (const website of websites) {
+    pipeline.xAdd(STREAM_NAME, "*", website);
+  }
+
+  return await pipeline.exec();
+}
